561cc3adbdd8751efb1b28078d7721c76b16b1f7
kaveeshaperera728 <104257370+pandaboi201@users.noreply.github.com>
2026-07-16 14:18:46 +0530

    Department save fixed

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -17,6 +17,9 @@ export const currentUser: User = {
 export const DEPARTMENT_OPTIONS = [
   "IT Operations", "Engineering", "Finance", "Human Resources", "Sales",
   "Marketing", "Security", "Facilities", "Support", "Legal",
+  "IT Division", "SLO Division", "Finance Division", "Marketing Division",
+  "Registrar's Office", "Software Division", "Computer Science Department",
+  "Management Department", "Psychology Department", "President's Office"
 ];
 
 export const LOCATION_OPTIONS = [
